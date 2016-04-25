8e9df3d07afbc8f6e83a549c075ad2bbaeb3ff42
EugenRakhimov <gin3002@gmail.com>
2016-04-26 11:35:26 +1200

    started access google api

--- app/Http/Controllers/SpreadsheetController.php
+++ app/Http/Controllers/SpreadsheetController.php
@@ -6,11 +6,22 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use Mitni\Google\GoogleSpreadsheet;
+
+use App\Product;
+use App\Feature;
+
 class SpreadsheetController extends Controller
 {
+
+
     public function load($value='')
     {
-      echo "hello world";
-      exit();
+      $path = base_path('credential.json');
+      $client = GoogleSpreadsheet::getClient($path);
+      $products_file = $client->file("13S44sM1RqxLOp0HSkTA0IPvIYsX02s1ie3QLoMAY9JY");
+      $features_file = $client->file("1jMWr23fPEf9UHZLRdZM2_ST5hqZhCMWYt0cTCkOOQpA");
+      // echo $client;
+      // exit();
     }
 }
